32877bfc8f051438b1c2444f4a963ad6c7669ad0
Chinhpham204719 <96942064+Chinhpham204719@users.noreply.github.com>
2024-01-18 21:19:39 +0700

    Update navTopConfigs.js

--- Frontend_IOT/src/configs/navTopConfigs.js
+++ Frontend_IOT/src/configs/navTopConfigs.js
@@ -1,1 +1,16 @@
+const navTopConfigs = [
+    {
+        id: "EcoGuard Monitor",
+        name:"EcoGuard Monitor",
+        navLink:"/",
+        item: []
+    },
+    {
+        id: "Devices",
+        name:"Devices",
+        navLink:"/devices",
+        item: []
+    }
+]
+export default navTopConfigs;
 
