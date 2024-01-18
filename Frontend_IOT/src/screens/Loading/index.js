1f6242a1492347430ad3b6dcc70a73904021d343
Chinhpham204719 <96942064+Chinhpham204719@users.noreply.github.com>
2024-01-18 21:31:31 +0700

    Create index.js

---
+++ Frontend_IOT/src/screens/Loading/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './style.css'
+export default function index() {
+  return (
+    <div>
+      <div class="bg flex">
+        <div class="c"></div>
+        <div class="line"></div>
+        <div class="r1"></div>
+        <div class="r2"></div>
+      </div>
+    </div>
+  )
+}
