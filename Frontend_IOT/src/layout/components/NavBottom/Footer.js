2ea143d519d7f8b979049d688cbab5a3f72b252e
Chinhpham204719 <96942064+Chinhpham204719@users.noreply.github.com>
2024-01-18 21:23:56 +0700

    Create Footer.js

---
+++ Frontend_IOT/src/layout/components/NavBottom/Footer.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import '../../style.css';
+import { Link } from 'react-router-dom';
+
+function Footer() {
+  return (
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+        Join our community to receive more information.
+        </p>
+        <p className='footer-subscription-text'>
+        Subscribe by email to receive the latest updates.
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Your Email'
+            />
+            
+          </form>
+        </div>
+      </section>
+      <div className='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>Social Media</h2>
+            <a href='https://www.facebook.com/profile.php?id=100024443408113/' target='_blank' rel='noopener noreferrer'>Facebook</a>
+            <a href='https://www.instagram.com/chinh_pham2411/' target='_blank' rel='noopener noreferrer'>Instagram</a>
+            <a href='https://twitter.com/chinhpham204719/' target='_blank' rel='noopener noreferrer'>Twitter</a>
+            <a href='https://www.youtube.com/channel/UCGRuAl40jxdxhMW_giMw50Q/' target='_blank' rel='noopener noreferrer'>Youtube</a>
+          </div>
+        </div>
+      </div>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <p className='website-rights'>DATN © 2024</p>
+          <div className='social-icons'>
+            <Link
+              className='social-icon-link facebook'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <i className='fab fa-facebook-f' />
+            </Link>
+            <Link
+              className='social-icon-link instagram'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <i className='fab fa-instagram' />
+            </Link>
+            <Link
+              className='social-icon-link youtube'
+              to='/'
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <i className='fab fa-youtube' />
+            </Link>
+            <Link
+              className='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <i className='fab fa-twitter' />
+            </Link>
+            <Link
+              className='social-icon-link twitter'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <i className='fab fa-linkedin' />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
