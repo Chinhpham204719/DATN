820837fc40e23b2cb7d3e8b0e7f8de257d5812e3
Chinhpham204719 <96942064+Chinhpham204719@users.noreply.github.com>
2024-01-18 21:48:28 +0700

    Create Hardware_IOT.ino

---
+++ Hardware_IOT/Hardware_IOT.ino
@@ -0,0 +1,232 @@
+#ifdef ESP8266
+ #include <ESP8266WiFi.h>
+ #else
+ #include <WiFi.h>
+#endif
+
+#include "DHT.h"
+#include <ArduinoJson.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
+#include <LiquidCrystal_I2C.h>
+
+#define DHTPIN 2 
+#define DHTTYPE DHT11
+DHT dht(DHTPIN, DHTTYPE);
+#define MQ_PIN A0
+
+const char* ssid = "B5-412";
+const char* password =  "doanket999";
+
+// HiveMQ Cloud parameters
+const char* mqtt_server =  "dea95a186d8943c3b6671f65a210e697.s2.eu.hivemq.cloud";
+const int mqtt_port = 8883;
+const char* mqtt_username = "esp8266"  ;
+const char* mqtt_password = "Aa123123";  
+
+/**** Secure WiFi Connectivity Initialisation *****/
+WiFiClientSecure espClient;
+
+/**** MQTT Client Initialisation Using WiFi Connection *****/
+PubSubClient client(espClient);
+
+unsigned long lastMsg = 0;
+#define MSG_BUFFER_SIZE (50)
+char msg[MSG_BUFFER_SIZE];
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+byte degree_symbol[8] = 
+{
+  0b00111,
+  0b00101,
+  0b00111,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000,
+  0b00000
+};
+
+/****** root certificate *********/
+
+static const char *root_ca PROGMEM = R"EOF(
+-----BEGIN CERTIFICATE-----
+MIIFazCCA1OgAwIBAgIRAIIQz7DSQONZRGPgu2OCiwAwDQYJKoZIhvcNAQELBQAw
+TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh
+cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMTUwNjA0MTEwNDM4
+WhcNMzUwNjA0MTEwNDM4WjBPMQswCQYDVQQGEwJVUzEpMCcGA1UEChMgSW50ZXJu
+ZXQgU2VjdXJpdHkgUmVzZWFyY2ggR3JvdXAxFTATBgNVBAMTDElTUkcgUm9vdCBY
+MTCCAiIwDQYJKoZIhvcNAQEBBQADggIPADCCAgoCggIBAK3oJHP0FDfzm54rVygc
+h77ct984kIxuPOZXoHj3dcKi/vVqbvYATyjb3miGbESTtrFj/RQSa78f0uoxmyF+
+0TM8ukj13Xnfs7j/EvEhmkvBioZxaUpmZmyPfjxwv60pIgbz5MDmgK7iS4+3mX6U
+A5/TR5d8mUgjU+g4rk8Kb4Mu0UlXjIB0ttov0DiNewNwIRt18jA8+o+u3dpjq+sW
+T8KOEUt+zwvo/7V3LvSye0rgTBIlDHCNAymg4VMk7BPZ7hm/ELNKjD+Jo2FR3qyH
+B5T0Y3HsLuJvW5iB4YlcNHlsdu87kGJ55tukmi8mxdAQ4Q7e2RCOFvu396j3x+UC
+B5iPNgiV5+I3lg02dZ77DnKxHZu8A/lJBdiB3QW0KtZB6awBdpUKD9jf1b0SHzUv
+KBds0pjBqAlkd25HN7rOrFleaJ1/ctaJxQZBKT5ZPt0m9STJEadao0xAH0ahmbWn
+OlFuhjuefXKnEgV4We0+UXgVCwOPjdAvBbI+e0ocS3MFEvzG6uBQE3xDk3SzynTn
+jh8BCNAw1FtxNrQHusEwMFxIt4I7mKZ9YIqioymCzLq9gwQbooMDQaHWBfEbwrbw
+qHyGO0aoSCqI3Haadr8faqU9GY/rOPNk3sgrDQoo//fb4hVC1CLQJ13hef4Y53CI
+rU7m2Ys6xt0nUW7/vGT1M0NPAgMBAAGjQjBAMA4GA1UdDwEB/wQEAwIBBjAPBgNV
+HRMBAf8EBTADAQH/MB0GA1UdDgQWBBR5tFnme7bl5AFzgAiIyBpY9umbbjANBgkq
+hkiG9w0BAQsFAAOCAgEAVR9YqbyyqFDQDLHYGmkgJykIrGF1XIpu+ILlaS/V9lZL
+ubhzEFnTIZd+50xx+7LSYK05qAvqFyFWhfFQDlnrzuBZ6brJFe+GnY+EgPbk6ZGQ
+3BebYhtF8GaV0nxvwuo77x/Py9auJ/GpsMiu/X1+mvoiBOv/2X/qkSsisRcOj/KK
+NFtY2PwByVS5uCbMiogziUwthDyC3+6WVwW6LLv3xLfHTjuCvjHIInNzktHCgKQ5
+ORAzI4JMPJ+GslWYHb4phowim57iaztXOoJwTdwJx4nLCgdNbOhdjsnvzqvHu7Ur
+TkXWStAmzOVyyghqpZXjFaH3pO3JLF+l+/+sKAIuvtd7u+Nxe5AW0wdeRlN8NwdC
+jNPElpzVmbUq4JUagEiuTDkHzsxHpFKVK7q4+63SM1N95R1NbdWhscdCb+ZAJzVc
+oyi3B43njTOQ5yOf+1CceWxG1bQVs5ZufpsMljq4Ui0/1lvh+wjChP4kqKOJ2qxq
+4RgqsahDYVvTH9w7jXbyLeiNdd8XM2w9U/t7y0Ff/9yi0GE44Za4rF2LN9d11TPA
+mRGunUHBcnWEvgJBQl9nJEiU0Zsnvgc/ubhPgXRR4Xq37Z0j4r7g1SgEEzwxA57d
+emyPxgcYxn/eR44/KJ4EBs+lVDR3veyJm+kXQ99b21/+jh5Xos1AnX5iItreGCc=
+-----END CERTIFICATE-----
+)EOF";
+
+/************* Connect to WiFi ***********/
+void setup_wifi() {
+  delay(10);
+  Serial.print("\nConnecting to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  randomSeed(micros());
+  Serial.println("\nWiFi connected\nIP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+/************* Connect to MQTT Broker ***********/
+void reconnect() {
+  // Loop until we're reconnected
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    String clientId = "ESP8266Client-";   // Create a random client ID
+    clientId += String(random(0xffff), HEX);
+    // Attempt to connect
+    if (client.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
+      Serial.println("connected");
+
+      client.subscribe("led_state");   // subscribe the topics here
+
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");   // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  String incommingMessage = "";
+  for (int i = 0; i < length; i++) incommingMessage+=(char)payload[i];
+
+  Serial.println("Message arrived ["+String(topic)+"]"+incommingMessage);
+
+}
+
+void publishMessage(const char* topic, String payload , boolean retained){
+  if (client.publish(topic, payload.c_str(), true))
+      Serial.println("Message publised ["+String(topic)+"]: "+payload);
+}
+
+void setup() {
+  //dht.setup(DHTpin, DHTesp::DHT11); 
+  pinMode(MQ_PIN, INPUT);
+  Serial.begin(115200);
+  while (!Serial) delay(1);
+  setup_wifi();
+
+  #ifdef ESP8266
+    espClient.setInsecure();
+  #else
+    espClient.setCACert(root_ca);      // enable this line and the the "certificate" code for secure connection
+  #endif
+
+  client.setServer(mqtt_server, mqtt_port);
+  client.setCallback(callback);
+    if (!client.connected()) {
+    Serial.print("Connecting to MQTT... ");
+    reconnect();
+  }
+
+  // Kiểm tra xem đã kết nối MQTT thành công hay không
+  if (client.connected()) {
+    Serial.println("Connected to MQTT");
+  } else {
+    Serial.println("Failed to connect to MQTT");
+  }
+}
+
+void loop() {
+  if (!client.connected()) reconnect(); // check if client is connected
+  client.loop();
+
+//read DHT11 temperature and humidity reading
+  delay(2000);
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  int gasvalue = analogRead(MQ_PIN);
+
+  DynamicJsonDocument doc(1024);
+
+  doc["deviceId"] = "ESP8266";
+  doc["siteId"] = "Environment Quality Kit";
+  doc["humidity"] = humidity;
+  doc["temperature"] = temperature;
+  doc["gasvalue"] = gasvalue;
+
+  char mqtt_message[128];
+  serializeJson(doc, mqtt_message);
+
+  publishMessage("esp8266_data", mqtt_message, true);
+    lcd.setCursor(0,0);
+  lcd.print("Temperature "); 
+  lcd.setCursor(0,1);
+  lcd.print(t);
+  lcd.setCursor(6,1);
+  lcd.write(1);
+  lcd.createChar(1, degree_symbol);
+  lcd.setCursor(7,1);
+  lcd.print("C");
+  delay(4000);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Humidity ");
+  lcd.print(h);
+  lcd.print("%");
+  delay(4000);
+  lcd.clear();
+
+  if(gasValue < 600)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Gas Value: ");
+    lcd.print(gasValue);
+    lcd.setCursor(0, 1);
+    lcd.print("Fresh Air");
+    Serial.println("Fresh Air");
+    delay(4000);
+    lcd.clear();
+  }
+  else if(gasValue > 600)
+  {
+    lcd.setCursor(0,0);
+    lcd.print("Gas Value: ");
+    lcd.print(gasValue);
+    lcd.setCursor(0, 1);
+    lcd.print("Bad Air");
+    Serial.println("Bad Air");
+    delay(4000);
+    lcd.clear();
+  }
+  delay(5000);
+
+}
